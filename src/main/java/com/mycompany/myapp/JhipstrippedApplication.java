30c5e78f590d22bbf3c0874f07c6e108c4cf4c28
Panos Vlastaridis <panosvlastaridis@gmail.com>
2015-03-22 07:35:12 +0200

    Commit 1 Just An STS Spring Starter Project

---
+++ src/main/java/com/mycompany/myapp/JhipstrippedApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhipstrippedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhipstrippedApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JhipstrippedApplication.class);
+	}
+
+}
